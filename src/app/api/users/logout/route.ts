c1d20ff8b59bf010cd32512b03bd8b00db2cf06f
divya562 <divyabagul2502@gmail.com>
2024-06-01 13:46:24 +0530

    d

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,10 +7,10 @@ export  async function GET(request: NextRequest){
             success:true
         })
         response.cookies.set("token","", {httpOnly:true, expires:new Date(0)})
-        localStorage.removeItem('token');
-        sessionStorage.removeItem('token');
+        
         return response;
     } catch (error:any) {
+        console.log(error.message)
         return NextResponse.json({error:error.message},
             {status:500}
         )
